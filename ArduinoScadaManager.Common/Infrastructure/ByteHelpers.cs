c8203dac421d8b6dec0e342833b4597daafd8ece
Szymon Bartnik <szymbar099@student.polsl.pl>
2015-04-27 01:51:21 +0200

    Added missing file.

---
+++ ArduinoScadaManager.Common/Infrastructure/ByteHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ArduinoScadaManager.Common.Infrastructure
+{
+    public static class ByteHelpers
+    {
+        public static byte[] StringToByteArray(this string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+
+        public static string ByteArrayToString(this byte[] ba)
+        {
+            return Encoding.ASCII.GetString(ba);
+        }
+
+        public static byte[] HexStringToByteArray(this string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string ByteArrayToHexString(this byte[] ba)
+        {
+            var hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
